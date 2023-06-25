a92fd3e020456054ecca09f8cd59f7a7df6c6082
Gustavo de los Santos <gusleo94@gmail.com>
2023-06-26 01:46:09 +0200

    feat: CORS added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(
+    title="PomoWork",
+    description="Una app para controlar pomodoros y cobros en distintos proyectos",
+)
+
+origins = [
+    # TODO: Configurar bien antes del deploy
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
