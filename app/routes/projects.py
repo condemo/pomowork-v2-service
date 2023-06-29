31a7f0eb0c4c2f63fd71ad6dc868d4af20be23f7
Gustavo de los Santos <gusleo94@gmail.com>
2023-06-29 21:05:39 +0200

    feat: basic projects post and get routes added

--- app/routes/projects.py
+++ app/routes/projects.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
 
+from app.dependencies.database import get_db
+from app.models import ProjectModel
+from app.schemas.projects import ProjectResponse, ProjectCreate
 
 router = APIRouter(
     prefix="/projects",
@@ -7,6 +11,21 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def root():
-    return {"message": "Projects Route"}
+@router.get("/", response_model=list[ProjectResponse])
+async def get_all_projects(db: Session = Depends(get_db)):
+    projects_list = db.query(ProjectModel).all()
+
+    return projects_list
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED,
+             response_model=ProjectResponse)
+async def create_project(project: ProjectCreate,
+                         db: Session = Depends(get_db)):
+    new_project = ProjectModel(**project.dict())
+    db.add(new_project)
+    db.commit()
+    db.refresh(new_project)
+    print(new_project.id)
+
+    return new_project
