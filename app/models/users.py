227ea32f5118b1e18e8caee6339b3e56e02ca71c
Gustavo de los Santos <gusleo94@gmail.com>
2023-07-02 22:04:58 +0200

    feat: email column added to users table

--- app/models/users.py
+++ app/models/users.py
@@ -9,3 +9,4 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
