a02a4636c165919244b89515d2c42b7f43579881
Gustavo de los Santos <gusleo94@gmail.com>
2023-07-03 20:47:42 +0200

    feat: password hash and verify functionality added

---
+++ app/utils/users.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def password_verify(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
